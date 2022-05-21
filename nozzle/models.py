e635d628b925f0a98ad4779c4238f8dc7126d8bc
Just Brian <brianmungai350@gmail.com>
2022-05-21 19:58:47 +0300

    Generated a file

--- nozzle/models.py
+++ nozzle/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class PDF(models.Model):
+    pdf_name = 'Nozzle Demensions'
+    slug = 'nozzle_dimensions'
+    generate_pdf = models.FileField()
+
+    def get_absolute_url(self):
+        return self.pdf_name
+    
+    def __str__(self):
+        return self.slug
--- nozzle/views.py
+++ nozzle/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 from math import sqrt, tan, radians, degrees, pi, exp
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from django.conf import settings
+from . models import PDF
 # Create your views here.
 
 class home(View):
@@ -70,7 +74,6 @@ class home(View):
             '''
             Processing the context
             '''
-            #global context
         context = {
             #inputs
             'thrust':F,
@@ -81,7 +84,8 @@ class home(View):
             'molecular_mass':M,
             'specific_heats_ratio':k,
             'chamber_length':Lstar,
-            'throttle_temperature':Tt,#outputs
+            'throttle_temperature':Tt,
+            #outputs
             'impulse':Isp,
             'effective_exhaust_velocity':v2,
             'mass_flow_rate':mdot,
@@ -101,6 +105,167 @@ class home(View):
             'diverging_nozzle_length':Ldn,
             'converging_nozzle_length':Lcn,
         }
+
+        #Create pdf data
+        #inputs
+        ff = F
+        chamber_p = P0
+        height = ALT
+        fuel_r = OF
+        chamber_t = T0
+        molecular_m = M
+        specific_h = k
+        chamber_l = Lstar
+        throttle_t = Tt#outputs
+        impul = Isp
+        effective_e = v2
+        mass_f = mdot
+        oxidizer_mass_f = mdot_oxidizer
+        fuel_m = mdot_fuel
+        exit_t = Te
+        exit_m = Mnum
+        pressure_r = PR
+        expansion_r = ER
+        #Nozzle dimensions
+        throat_a = At
+        exit_a = Ae
+        throat_r = Rt
+        exit_r = Re
+        chamber_r = R,
+        chamber_l = Lc
+        diverging_nozzle_l = Ldn
+        converging_nozzle_l = Lcn
+
+        # Setting style for bar graphs
+        # set font
+        plt.rcParams['font.family'] = 'sans-serif'
+        plt.rcParams['font.sans-serif'] = 'STIXGeneral'
+        fig, ax = plt.subplots(figsize=(10, 14.5))
+        # remove axes
+        #plt.axis('off')
+        name = 'Nozzle dimensions'
+        inputs = 'Inputs You provided'
+        thrust = 'thrust'
+        cp = 'chamber_pressure'
+        alt = 'altitude'
+        fuel = 'fuel_ratio'
+        cp = 'chamber_temparature'
+        mass = 'molecular_mass'
+        shr = 'specific_heats_ratio'
+        cl = 'chamber_length'
+        tt = 'throttle_temperature'
+        ev = 'Effective_exhaust_velocity :'
+
+        outputs = 'Relevant Outputs'
+        imp = 'impulse'
+        eev = 'effective_exhaust_velocity'
+        flow_rate = 'mass_flow_rate'
+        oxmfr = 'oxidizer_mass_flow_rate'
+        fmfr = 'fuel_mass_flow_rate'
+        et = 'exit_temparature'
+        mach = 'exit_mach_number'
+        pa = 'pressure_ratio'
+        er = 'expansion_ratio'
+
+        ta = 'throat_area'
+        ea = 'exit_area'
+        tr = 'throat_radius'
+        erad = 'exit_radius'
+        cr = 'chamber_radius'
+        cl = 'chamber_length'
+        dnl = 'diverging_nozzle_length'
+        cnl = 'converging_nozzle_length'
+        dime = 'Calculated Nozzle Dimensions'
+        #Header
+        plt.annotate(name, (0.02, 0.94), weight='bold', fontsize=20)
+        plt.annotate(inputs, (0.02, 0.88), weight='bold', fontsize=20)
+        #Input Identifiers
+        plt.annotate(thrust, (0.05, 0.84), weight='regular', fontsize=12)
+        plt.annotate(cp, (0.05, 0.82), weight='regular', fontsize=12)
+        plt.annotate(alt, (0.05, 0.8), weight='regular', fontsize=12)
+        plt.annotate(fuel, (0.05, 0.78), weight='regular', fontsize=12)
+        plt.annotate(cp, (0.05, 0.76), weight='regular', fontsize=12)
+        plt.annotate(mass, (0.05, 0.74), weight='regular', fontsize=12)
+        plt.annotate(shr, (0.05, 0.72), weight='regular', fontsize=12)
+        plt.annotate(cl, (0.05, 0.7), weight='regular', fontsize=12)
+        plt.annotate(tt, (0.05, 0.68), weight='regular', fontsize=12)
+        plt.annotate(ev, (0.05, 0.66), weight='regular', fontsize=12)
+        #Input values
+        plt.annotate(ff, (0.5, 0.84), weight='regular', fontsize=12)
+        plt.annotate(chamber_p, (0.5, 0.82), weight='regular', fontsize=12)
+        plt.annotate(height, (0.5, 0.8), weight='regular', fontsize=12)
+        plt.annotate(fuel, (0.5, 0.78), weight='regular', fontsize=12)
+        plt.annotate(fuel_r, (0.5, 0.76), weight='regular', fontsize=12)
+        plt.annotate(chamber_t, (0.5, 0.74), weight='regular', fontsize=12)
+        plt.annotate(molecular_m, (0.5, 0.72), weight='regular', fontsize=12)
+        plt.annotate(specific_h, (0.5, 0.7), weight='regular', fontsize=12)
+        plt.annotate(chamber_l, (0.5, 0.68), weight='regular', fontsize=12)
+        plt.annotate(throttle_t, (0.5, 0.66), weight='regular', fontsize=12)
+        #outputs
+
+        plt.annotate(outputs, (.02, 0.62), weight='bold', fontsize=20)
+        #Identifiers (Variables)
+        plt.annotate(imp, (0.05, 0.58), weight='regular', fontsize=12)
+        plt.annotate(eev, (0.05, 0.56), weight='regular', fontsize=12)
+        plt.annotate(flow_rate, (0.05, 0.54), weight='regular', fontsize=12)
+        plt.annotate(oxmfr, (0.05, 0.52), weight='regular', fontsize=12)
+        plt.annotate(fmfr, (0.05, 0.5), weight='regular', fontsize=12)
+        plt.annotate(et, (0.05, 0.48), weight='regular', fontsize=12)
+        plt.annotate(mach, (0.05, 0.46), weight='regular', fontsize=12)
+        plt.annotate(pa, (0.05, 0.44), weight='regular', fontsize=12)
+        plt.annotate(er, (0.05, 0.42), weight='regular', fontsize=12)
+        #Values of output
+        plt.annotate(impul, (0.5, 0.58), weight='regular', fontsize=12)
+        plt.annotate(effective_e, (0.5, 0.56), weight='regular', fontsize=12)
+        plt.annotate(mass_f, (0.5, 0.54), weight='regular', fontsize=12)
+        plt.annotate(oxidizer_mass_f, (0.5, 0.52), weight='regular', fontsize=12)
+        plt.annotate(fuel_m, (0.5, 0.5), weight='regular', fontsize=12)
+        plt.annotate(exit_t, (0.5, 0.48), weight='regular', fontsize=12)
+        plt.annotate(exit_m, (0.5, 0.46), weight='regular', fontsize=12)
+        plt.annotate(pressure_r, (0.5, 0.44), weight='regular', fontsize=12)
+        plt.annotate(expansion_r, (0.5, 0.42), weight='regular', fontsize=12)
+
+        #Approx. Calculated Nozzle Dimensions
+
+        plt.annotate(dime, (.02,0.38), weight='bold', fontsize=20)
+        #Dimensions variables
+        plt.annotate(ta, (0.05, 0.34), weight='regular', fontsize=12)
+        plt.annotate(ea, (0.05, 0.32), weight='regular', fontsize=12)
+        plt.annotate(tr, (0.05, 0.3), weight='regular', fontsize=12)
+        plt.annotate(erad, (0.05, 0.28), weight='regular', fontsize=12)
+        plt.annotate(cr, (0.05, 0.26), weight='regular', fontsize=12)
+        plt.annotate(cl, (0.05, 0.24), weight='regular', fontsize=12)
+        plt.annotate(dnl, (0.05, 0.22), weight='regular', fontsize=12)
+        plt.annotate(cnl, (0.05, 0.2), weight='regular', fontsize=12)
+        #Dime Values
+        plt.annotate(throat_a, (0.5, 0.34), weight='regular', fontsize=12)
+        plt.annotate(exit_a, (0.5, 0.32), weight='regular', fontsize=12)
+        plt.annotate(throat_r, (0.5, 0.3), weight='regular', fontsize=12)
+        plt.annotate(exit_r, (0.5, 0.28), weight='regular', fontsize=12)
+        plt.annotate(chamber_r, (0.5, 0.26), weight='regular', fontsize=12)
+        plt.annotate(chamber_l, (0.5, 0.24), weight='regular', fontsize=12)
+        plt.annotate(diverging_nozzle_l, (0.5, 0.22), weight='regular', fontsize=12)
+        plt.annotate(converging_nozzle_l, (0.5, 0.2), weight='regular', fontsize=12)
+        plt.axis('off')
+
+        #Setting the path..
+        filename = 'Nozzle Dimensions.pdf'
+        file_path = '%s%s' % (settings.MEDIA_ROOT, filename)
+        plt.savefig(file_path, dpi=300, bbox_inches='tight')
+
+
+        #Adding the generated file to db
+        x = PDF.objects.create()
+        x.save()
+
+
+
+
+
+
+
+
+
 
 
         return render(request, self.template_name, context)
