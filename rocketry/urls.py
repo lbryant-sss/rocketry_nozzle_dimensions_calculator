37c90c343b2d74562f141dea82fd22ae9a7ee449
lbryant-sss <brianmungai350@gmail.com>
2024-04-20 13:13:51 +0300

    Heroku Procfile

--- rocketry/urls.py
+++ rocketry/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('nozzle.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
